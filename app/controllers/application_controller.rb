64dd11359c2a7d41a9aa34fefbdc6cbdcebec59e
Sheryl <sherylnome@example.com>
2023-07-03 15:43:25 +0000

    howru is added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
     render html: "Hello, World!"
   end
 
+  def howareyou
+    render html: "How are you today?"
+  end
+
   def goodbye
     render html: "Goodbye, World!"
   end
